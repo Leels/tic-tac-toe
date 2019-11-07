f8a409b85ed78dd9df77b964c0a6de9129ca8a1c
Student <example@example.com>
2019-11-07 15:57:36 -0800

    added some stuff

---
+++ README.md
@@ -0,0 +1,8 @@
+It will:
+
+1. Have two players
+2. Have 9 spaces
+3. Identify "3 in a row"
+4. 3 i a row from the same person
+5. change turns from one player to another
+6. 
---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+function Game() {
+  this.players= [],
+  this.board= [],
+  this.turn= turn,
+  this.spaces= ["11", "12", "13", "21", "22", "23", "31", "32", "33"]
+}
+
+function Player() {
+  this.mark= mark,
+  // this.score= []
+}
+
+Game.prototype.switchPlayer = function() {
+  if (this.currentPlayer === "X" {
+    this.currentPlayer = "O";
+  } else {
+    this.currentPlayer = "X"
+  }
+};
+
+
+
+
+$(document).ready(function() {
+  $("button").click()
+
+// if button clicked
+//check id
+
+
+
+})
